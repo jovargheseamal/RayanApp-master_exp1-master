9c03a00b47b4ba8e719859ae1848a1bb4cc72ed9
jovargheseamal <hemlockam@gmail.com>
2018-12-22 16:49:01 +0530

    test

--- app/src/main/java/com/example/user123/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/user123/myapplication/HomeActivity.java
@@ -26,7 +26,7 @@ public class HomeActivity extends BaseActivity implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
 
 
-        // test pulling
+        // test pulling frm lap
 
 
 
